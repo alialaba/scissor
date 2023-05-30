bc37d7528022a372d7505d3ab640ee08bb33b01d
alialaba <alialaba079@gmail.com>
2023-05-30 18:45:58 -0400

    setup server

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,10 @@
-export {};
+import express from "express";
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+    res.send("Hello NodeTypscript");
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,13 @@
-import express from "express"
+import express from "express";
 const app = express();
+const port = 3000;
+
+
+app.get("/", (req,res)=>{
+    res.send("Hello NodeTypscript")
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
