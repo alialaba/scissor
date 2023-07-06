f340365fb63084cd5dca8bf1061d58e45a62bac4
alialaba <alialaba079@gmail.com>
2023-07-06 09:42:22 -0400

    rm dist file

--- dist/Routes/Auth.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { signup, login } from '../Controllers/Auth.js';
-router.route("/signup").post(signup);
-router.route("/login").post(login);
-export default router;
-//# sourceMappingURL=Auth.js.map
--- dist/middlewares/rateLimter.js
+++
@@ -1,8 +0,0 @@
-import rateLimit from 'express-rate-limit';
-export const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    standardHeaders: true,
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
-//# sourceMappingURL=rateLimter.js.map
